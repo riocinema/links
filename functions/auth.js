0fc737185751932f3f4213c75e3243853e1a677d
riocinema <digital@riocinema.org.uk>
2026-06-10 15:48:39 +0100

    Update auth.js

--- functions/auth.js
+++ functions/auth.js
@@ -1,1 +1,12 @@
+export async function onRequestGet({ env }) {
+  const params = new URLSearchParams({
+    client_id: env.GITHUB_CLIENT_ID,
+    scope: "repo,user",
+    redirect_uri: "https://links-dvm.pages.dev/callback"
+  });
 
+  return Response.redirect(
+    `https://github.com/login/oauth/authorize?${params.toString()}`,
+    302
+  );
+}
