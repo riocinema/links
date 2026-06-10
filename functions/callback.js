5c6b06bb0e23bc693250e1ab8adfc1b2821a05e8
riocinema <digital@riocinema.org.uk>
2026-06-10 15:49:00 +0100

    Update callback.js

--- functions/callback.js
+++ functions/callback.js
@@ -1,1 +1,36 @@
 
+export async function onRequestGet({ request, env }) {
+  const url = new URL(request.url);
+  const code = url.searchParams.get("code");
+
+  if (!code) {
+    return new Response("Missing GitHub code", { status: 400 });
+  }
+
+  const tokenResponse = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      client_id: env.GITHUB_CLIENT_ID,
+      client_secret: env.GITHUB_CLIENT_SECRET,
+      code
+    })
+  });
+
+  const tokenData = await tokenResponse.json();
+
+  return new Response(`
+    <script>
+      window.opener.postMessage(
+        'authorization:github:success:${JSON.stringify(tokenData)}',
+        '*'
+      );
+      window.close();
+    </script>
+  `, {
+    headers: { "Content-Type": "text/html" }
+  });
+}
